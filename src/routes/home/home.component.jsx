a1f9441139760b38fe933ff916f8ce3efb48f542
Souhardya Samanta <samantasouhardya14285@gmail.com>
2024-01-27 20:51:55 +0530

    Udated folder name

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import Directory from '../../components/directory/directory.component';
+import Directory from '../../components/Directory/directory.component';
 
 const Home = () => {
   return (
